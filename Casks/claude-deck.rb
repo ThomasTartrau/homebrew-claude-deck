9b307cdd3c3073d223f7a40274ed519babe6bc9b
Thomas Tartrau <145434513+ThomasTartrau@users.noreply.github.com>
2026-03-08 21:18:49 +0000

    Update Claude Deck App to v0.1.9

--- Casks/claude-deck.rb
+++ Casks/claude-deck.rb
@@ -1,9 +1,9 @@
 cask "claude-deck" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.1.8"
-  sha256 arm:   "0fd1fd5c4433ba534ea995fd1c200d4d7a64da306517a0c3b08ede335107919e",
-         intel: "ecaf5423558bf00580ab210f1e8536fefdde508a33edc481c84b74bce93d98a0"
+  version "0.1.9"
+  sha256 arm:   "4f3e0f21e2e506e7971de6cbc66696b9f9350180e72049f019e53cb29db4a8ea",
+         intel: "d2e23cb5ef49bed395ca8557283e35cde2209016b198d7a979d8beb5b3b37453"
 
   url "https://github.com/ThomasTartrau/claude-deck/releases/download/app-v#{version}/Claude.Deck_#{version}_#{arch}.dmg"
   name "Claude Deck"
